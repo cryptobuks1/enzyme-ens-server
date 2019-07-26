4b66fbc1ad16c8f67435c347ae52d0d6ff416db1
Ivan Herger <ivan.herger@gmail.com>
2019-07-26 17:59:18 +0200

    Experimenting with axios

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
 import { NowRequest, NowResponse } from "@now/node";
-import axios from "axios";
-
-const coinApi = axios.create({
-  baseURL: "https://rest.coinapi.io",
-  headers: {
-    "X-CoinAPI-Key": "73034021-0EBC-493D-8A00-E0F138111F41"
-    // "X-CoinAPI-Key": process.env.COINAPI_KEY
-  }
-});
+import Axios from "axios";
 
 export default async function(req: NowRequest, res: NowResponse) {
   const { name = "World" } = req.query;
+
+  const coinApi = Axios.create({
+    baseURL: "https://rest.coinapi.io",
+    headers: {
+      "X-CoinAPI-Key": "73034021-0EBC-493D-8A00-E0F138111F41"
+      // "X-CoinAPI-Key": process.env.COINAPI_KEY
+    }
+  });
 
   const response = await coinApi.get(`/v1/exchangerate/MLN/ETH`);
 
