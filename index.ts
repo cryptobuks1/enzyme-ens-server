5081d7b167faeb4f8c5922c1e97abdbd43ffb456
Ivan Herger <ivan.herger@gmail.com>
2019-07-26 16:38:58 +0200

    Add cache control headers

--- index.ts
+++ index.ts
@@ -2,5 +2,6 @@ import { NowRequest, NowResponse } from "@now/node";
 
 export default function(req: NowRequest, res: NowResponse) {
   const { name = "World" } = req.query;
+  res.setHeader("Cache-Control", "s-maxage=600");
   res.send(`Hello ${name}!`);
 }
