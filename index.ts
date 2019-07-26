1fc31528807d0decc2e76a1633dc9232691b5fa7
Ivan Herger <ivan.herger@gmail.com>
2019-07-26 17:37:12 +0200

    Add axios for CoinAPI requests

--- index.ts
+++ index.ts
@@ -1,7 +1,25 @@
 import { NowRequest, NowResponse } from "@now/node";
+import axios from "axios";
 
-export default function(req: NowRequest, res: NowResponse) {
+const coinApi = axios.create({
+  baseURL: "https://rest.coinapi.io",
+  headers: {
+    "X-CoinAPI-Key": "73034021-0EBC-493D-8A00-E0F138111F41"
+    // "X-CoinAPI-Key": process.env.COINAPI_KEY
+  }
+});
+
+export default async function(req: NowRequest, res: NowResponse) {
   const { name = "World" } = req.query;
-  res.setHeader("Cache-Control", "s-maxage=600");
-  res.send(`Hello ${name}!`);
+
+  const response = await coinApi.get(`/v1/exchangerate/MLN/ETH`);
+
+  if (!!response.data.error) {
+    throw new Error(response.data.error);
+  }
+
+  const rate = response.data && response.data.rate;
+
+  res.setHeader("Cache-Control", "maxage=0, s-maxage=600");
+  res.send(`Hello ${name}! Rate: ${rate}`);
 }
