655089dc8a8a12dba412326d330aa0765df63505
Ivan Herger <ivan.herger@gmail.com>
2019-07-26 18:02:17 +0200

    Experimenting with axios

--- index.ts
+++ index.ts
@@ -5,21 +5,17 @@ export default async function(req: NowRequest, res: NowResponse) {
   const { name = "World" } = req.query;
 
   const coinApi = Axios.create({
-    baseURL: "https://rest.coinapi.io",
-    headers: {
-      "X-CoinAPI-Key": "73034021-0EBC-493D-8A00-E0F138111F41"
-      // "X-CoinAPI-Key": process.env.COINAPI_KEY
-    }
+    baseURL: "https://swapi.co/api"
   });
 
-  const response = await coinApi.get(`/v1/exchangerate/MLN/ETH`);
+  const response = await coinApi.get(`/people/1`);
 
   if (!!response.data.error) {
     throw new Error(response.data.error);
   }
 
-  const rate = response.data && response.data.rate;
+  const data = response.data;
 
   res.setHeader("Cache-Control", "maxage=0, s-maxage=600");
-  res.send(`Hello ${name}! Rate: ${rate}`);
+  res.send(`Hello ${name}! Rate: ${data}`);
 }
